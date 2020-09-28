4ff326097760ee26be4ec8623bb3e16523421649
nelson-singleton <nsingleton@ymail.com>
2020-09-28 17:24:33 -0500

    Post and delete requests are working properly

--- index.js
+++ index.js
@@ -23,16 +23,27 @@ server.get('/api/users/:id', (req, res) => {
     const id = Number(req.params.id)
     const foundById = users.filter( user => user.id == id)    
 
-    //if (foundById) {
+    if (foundById) {
         res.status(200).json({ data: foundById })
-    //}
-
+    } else {
+        res.status(404).json({ message: "Coudln't find a user with that ID."})
+    }
 })
 /////////////////////////////////////////////////////////////////// Post
 server.post('/api/users', (req, res) => {
     const postData = req.body
+    let nextId = 2
 
+    users.push({...postData, id: nextId++})
 
+    res.status(201).json({data: postData, users})
+})
+////////////////////////////////////////////////////////////////// Delete
+server.delete('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const foundById = users.filter( user => user.id !== id)
+    
+    res.status(200).json({data: users})
 })
 
 
